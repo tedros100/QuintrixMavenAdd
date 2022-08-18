7ce14c77b82b9e748326023b969c53872d0f8944
tedrosberihu <teddykiros123@gmail.com>
2022-08-18 16:56:01 -0700

    first Project

---
+++ src/main/java/com/quintrix/maven/java/Maven/CountWords.java
@@ -0,0 +1,52 @@
+package com.quintrix.maven.java.Maven;
+
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class CountWords {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map<String, Integer> wordMap = new TreeMap<String,Integer>();
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader("mobydick.txt"));
+			
+			
+			String line = "";
+			while (bf.readLine()!=null) {
+				line = bf.readLine();
+				//line.replaceAll("\\p{P}|\\`|[0-9]|\\r\n", " ").toLowerCase().trim(), "\\s* \\s*");
+				String words[] =line.replaceAll("\\p{P}|\\'|[0-9]|\\r\n", " ").split(" ");
+				for(int i=0; i<words.length;i++) {
+					if(wordMap.containsKey(words[i])) {
+						wordMap.put(words[i], wordMap.get(words[i])+1);
+					} else {
+					wordMap.put(words[i], 1);
+					}
+				}
+				
+			}
+					
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (String key: wordMap.keySet()) {
+			System.out.println(key+"\t"+wordMap.get(key));
+		}
+		
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String,Integer>>(wordMap.entrySet());
+		Collections.sort(list, (o1, o2)  -> o1.getValue().compareTo(o2.getValue()));
+		System.out.println(list);
+		
+		
+
+	}
+
+}
